449682de987e68bb55a649e88fc3b162cf2907d9
sunil-gudle <sunilgudle7844@gmail.com>
2026-03-16 13:11:39 +0530

    March-16-2026 Added programs on arrays commit 1

--- src/main/java/arrays/PrintTwoDimensionalArray.java
+++ src/main/java/arrays/PrintTwoDimensionalArray.java
@@ -4,11 +4,11 @@ package arrays;
 
 public class PrintTwoDimensionalArray {
     public static void main(String[] args) {
-        int [][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
         // using for loop
-        for (int i =0; i < arr.length;i++){
-            for (int j = 0; j < arr[0].length ; j++){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
@@ -17,8 +17,8 @@ public class PrintTwoDimensionalArray {
         System.out.println("\nBy using for-each loop");
 
         // using for-each loop
-        for (int[] x : arr){
-            for (int y : x){
+        for (int[] x : arr) {
+            for (int y : x) {
                 System.out.print(y + " ");
             }
             System.out.println();
